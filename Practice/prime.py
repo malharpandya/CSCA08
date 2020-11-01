e46426e2651763080c25045a7e15fb16897d7363
malharpandya <41825230+malharpandya@users.noreply.github.com>
2020-11-01 14:26:24 -0500

    Create prime.py

---
+++ Practice/prime.py
@@ -0,0 +1,12 @@
+import math
+def f():
+    n = int(input("enter number to be checked:"))
+    c = 1
+    for i in range (2,math.floor(math.sqrt(n))):
+        if n%i == 0:
+            c = 0
+            break
+    if c == 0 :
+        print(n + " is not prime")
+    else:
+        print (n + " is prime")
