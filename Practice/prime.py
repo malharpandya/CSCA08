6e962442d2884b01c806b8c1105ddd4d72594a43
malharpandya <41825230+malharpandya@users.noreply.github.com>
2020-11-01 14:30:55 -0500

    Update prime.py

--- Practice/prime.py
+++ Practice/prime.py
@@ -3,10 +3,10 @@ def f():
     n = int(input("enter number to be checked:"))
     c = 1
     for i in range (2,math.floor(math.sqrt(n))):
-        if n%i == 0:
+        if n % i == 0:
             c = 0
             break
     if c == 0 :
-        print(n + " is not prime")
+        print(n," is not prime")
     else:
-        print (n + " is prime")
+        print (n," is prime")
